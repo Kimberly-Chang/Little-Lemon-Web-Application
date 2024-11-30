86d577aaaf4a0ac6e1e90d624795a08cf431952a
Kimberly C. <kc.KimberlyChang@gmail.com>
2024-11-29 21:31:50 -0500

    Set Up API Views
    Added serializers for User, Booking, and Menu models. Added views for the Menu model. Added viewsets for the User and Booking models. Added API endpoints for the Booking viewset and Menu views.

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
+from .models import Booking, Menu
+from .serializers import UserSerializer, BookingSerializer, MenuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class UserViewSet(ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [IsAuthenticated] 
+        
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+        
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
